9be35077aadacef4959ab55061e17a288296c326
sammed-21 <sammed2110@gmail.com>
2023-08-16 12:58:00 +0530

    first commit

---
+++ src/app/data.ts
@@ -0,0 +1,21 @@
+const customers = [
+    {
+      id: 1,
+      name: "John Doe",
+      email: "john@example.com",
+      phoneNumber: "123-456-7890",
+    },
+    {
+      id: 2,
+      name: "Jane Smith",
+      email: "jane@example.com",
+      phoneNumber: "987-654-3210",
+    },
+   
+  ];
+  
+  export default customers;
+ 
+  
+  
+  
